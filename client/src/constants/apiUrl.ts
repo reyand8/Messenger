a403ae45ce04266249d55830e4ed94f416b802ce
Andrey <andreeey.gg@gmail.com>
2024-11-12 08:38:51 +0100

    add constants

---
+++ client/src/constants/apiUrl.ts
@@ -0,0 +1,7 @@
+const BASE_URL: string = 'http://localhost:5001'
+
+export const API_USERS_URL: string = `${BASE_URL}/api/users`;
+
+export const API_AUTH_URL: string = `${BASE_URL}/api/auth`;
+
+export const API_MESSAGES_URL: string = `${BASE_URL}/api/messages`;
