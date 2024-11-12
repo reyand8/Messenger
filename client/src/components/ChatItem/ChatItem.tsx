3bfb82516e6ab1162cc4f857de6ec6b91d68f5ae
Andrey <andreeey.gg@gmail.com>
2024-11-12 08:40:54 +0100

    add ChatItem

---
+++ client/src/components/ChatItem/ChatItem.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import getRandomContact from "../../utils/randomImg";
+import {IChatItemProps} from "../../types/props/props.interface";
+
+
+const ChatItem: React.FC<IChatItemProps> = ({user, setSelectedFriend}) => {
+    return (
+        <div onClick={() => setSelectedFriend(user)} key={user.id}
+             className="flex justify-between items-center p-3 hover:bg-gray-800 rounded-lg relative">
+            <div className="flex flex-shrink-0 w-16 h-16 relative">
+                <img
+                    className="shadow-md rounded-full w-full h-full object-cover"
+                    src={getRandomContact()} alt=""
+                />
+            </div>
+            <div className="flex-auto min-w-0 ml-5 mr-6 hidden md:block group-hover:block">
+                <p className="text-white font-semibold">{user.username}</p>
+            </div>
+        </div>
+    )
+}
+
+export default ChatItem;
