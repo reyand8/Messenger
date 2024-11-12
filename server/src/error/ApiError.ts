64a9be04025d18c1942d58f784796b9ac13031f9
Andrey <andreeey.gg@gmail.com>
2024-11-12 08:32:09 +0100

    add errors

---
+++ server/src/error/ApiError.ts
@@ -0,0 +1,20 @@
+import {IApiError} from "../types/error.interface";
+
+
+const ApiError = (status: number, message: string): IApiError => {
+    return { status, message };
+};
+
+const badRequest = (message: string): IApiError => {
+    return ApiError(404, message);
+};
+
+const internal = (message: string): IApiError => {
+    return ApiError(500, message);
+};
+
+const forbidden = (message: string): IApiError => {
+    return ApiError(403, message);
+};
+
+export { ApiError, badRequest, internal, forbidden };
