f138dc43dee044776372b70e93c44c411d582cd8
Andrey <andreeey.gg@gmail.com>
2024-11-14 09:20:32 +0100

    add tests

---
+++ server/src/controllers/userController.test.ts
@@ -0,0 +1,58 @@
+import { getUsers } from './userController';
+import { User } from '../models/User';
+import { Request, Response } from 'express';
+
+jest.mock('../models/User');
+
+describe('User Controller', () => {
+    let req: Partial<Request>;
+    let res: Partial<Response>;
+    let jsonMock: jest.Mock;
+    let statusMock: jest.Mock;
+
+    beforeEach(() => {
+        jsonMock = jest.fn();
+        statusMock = jest.fn().mockReturnValue({ json: jsonMock });
+
+        req = {};
+        res = {
+            status: statusMock,
+            json: jsonMock,
+        };
+
+        jest.clearAllMocks();
+    });
+
+    describe('getUsers', () => {
+        it('should return a list of users', async () => {
+
+            const mockUsers = [
+                { id: 1, username: 'user one' },
+                { id: 2, username: 'user two' },
+            ];
+            (User.findAll as jest.Mock).mockResolvedValue(mockUsers);
+
+            await getUsers(req as Request, res as Response);
+
+            expect(User.findAll).toHaveBeenCalledWith({
+                attributes: ['id', 'username'],
+            });
+
+            expect(jsonMock).toHaveBeenCalledWith(mockUsers);
+        });
+
+        it('should return a database error', async () => {
+            const errorMessage = new Error('Database error');
+            (User.findAll as jest.Mock).mockRejectedValue(errorMessage);
+
+            await getUsers(req as Request, res as Response);
+
+            expect(statusMock).toHaveBeenCalledWith(500);
+
+            expect(jsonMock).toHaveBeenCalledWith({
+                message: 'Get users error',
+                error: errorMessage,
+            });
+        });
+    });
+});
