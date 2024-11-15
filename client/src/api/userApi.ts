4290bb89b52555e4508b2045ef8cca5208f721b3
Andrey <andreeey.gg@gmail.com>
2024-11-15 11:37:40 +0100

    update api files

--- client/src/api/userApi.ts
+++ client/src/api/userApi.ts
@@ -1,17 +1,22 @@
 import axios from 'axios';
 
 import {API_USERS_URL} from '../constants/apiUrl';
+import {IUser} from '../types/user/user.interface';
+
+export type IUserList = IUser[];
 
 /**
  * Fetches all users from the server by sending a GET request.
  *
  * @returns {Promise<any>} The response data from the server, usually an array of users.
  */
-export const fetchAllUsers = async (): Promise<any> => {
+export const fetchAllUsers = async (): Promise<IUserList | null> => {
     try {
         const response = await axios.get(`${API_USERS_URL}/allUsers`);
-        return response.data;
+        console.log(response.data);
+        return response.data as IUserList;
     } catch (err) {
         console.error(err);
+        return null;
     }
 };
