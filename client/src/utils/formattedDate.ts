667d5a97b980d82cba9d26a0b76969a72dcf8e85
Andrey <andreeey.gg@gmail.com>
2024-11-15 11:51:41 +0100

    format data without 'date-fns'

--- client/src/utils/formattedDate.ts
+++ client/src/utils/formattedDate.ts
@@ -1,6 +1,10 @@
-import { format } from 'date-fns';
-
 export const formattedDate = (date: string): string => {
     const newDate = new Date(date);
-    return format(newDate, 'd/M/yy, HH:mm');
+    const day = newDate.getDate();
+    const month = newDate.getMonth() + 1;
+    const year = newDate.getFullYear().toString().slice(-2);
+    const hours = String(newDate.getHours()).padStart(2, '0');
+    const minutes = String(newDate.getMinutes()).padStart(2, '0');
+
+    return `${day}/${month}/${year}, ${hours}:${minutes}`;
 };
